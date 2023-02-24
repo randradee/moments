d0449012eed84e45214ded21ec8fe76ae4bed739
Renato Andrade <dev.renatopandrade@gmail.com>
2023-02-23 21:20:53 -0300

    realizadas validações de formulário

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
@@ -7,4 +7,32 @@ import { Component, Input } from '@angular/core';
 })
 export class MomentFormComponent {
   @Input() btnText!: string;
+
+  momentForm!: FormGroup;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return;
+    }
+    console.log('form enviado');
+  }
 }
